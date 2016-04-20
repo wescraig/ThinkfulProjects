64ce442a38bdc584502081335e667ec970672d83
wescraig <wescraig93@gmail.com>
2016-04-20 10:28:09 -0500

    Controller and index.html finalized

--- controllers.js
+++ controllers.js
@@ -7,8 +7,40 @@ angular.module('madLibApp', [])
 	this.maleName = "";
 	this.maleNameOutput = "________________";
 
+	this.jobTitle = "";
+	this.jobTitleOutput = "________________";
+
+	this.tedTask = "";
+	this.tedTaskOutput = "________________";
+
+	this.dirtyTask = "";
+	this.dirtyTaskOutput = "________________";
+
+	this.celeb = "";
+	this.celebOutput = "________________";
+
+	this.uselessSkill = "";
+	this.uselessSkillOutput = "________________";
+
+	this.obCeleb = "";
+	this.obCelebOutput = "________________";
+
+	this.hugeNum = "";
+	this.hugeNumOutput = "________________";
+
+	this.adj = "";
+	this.adjOutput = "________________";	
+
 	this.applyWords = function(){
 		this.maleNameOutput = this.maleName;
+		this.jobTitleOutput = this.jobTitle;
+		this.tedTaskOutput = this.tedTask;
+		this.dirtyTaskOutput = this.dirtyTask;
+		this.celebOutput = this.celeb
+		this.uselessSkillOutput = this.uselessSkill;
+		this.obCelebOutput = this.obCeleb;
+		this.hugeNumOutput = this.hugeNum;
+		this.adjOutput = this.adj;
 	}
 
 });
