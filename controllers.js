2c4c17be090e328a3e85484962b55c53fd9caffe
wescraig <wescraig93@gmail.com>
2016-04-23 10:11:47 -0500

    Update to Mad Libs with user validation.

--- controllers.js
+++ controllers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('madLibApp', [])
+angular.module('madLibApp', ['ngMessages'])
 
 .controller('madLibCtrl', function(){
 	
@@ -29,9 +29,12 @@ angular.module('madLibApp', [])
 	this.hugeNumOutput = "________________";
 
 	this.adj = "";
-	this.adjOutput = "________________";	
+	this.adjOutput = "________________";
 
-	this.applyWords = function(){
+	this.isFormValid = true;
+	this.errorClass = "";	
+
+	this.applyWords = function(form){
 		this.maleNameOutput = this.maleName;
 		this.jobTitleOutput = this.jobTitle;
 		this.tedTaskOutput = this.tedTask;
@@ -41,6 +44,42 @@ angular.module('madLibApp', [])
 		this.obCelebOutput = this.obCeleb;
 		this.hugeNumOutput = this.hugeNum;
 		this.adjOutput = this.adj;
+		this.isFormValid = form.$submitted && form.$valid;
+	}
+
+	this.resetForm = function(form){
+		this.maleName = "";
+		this.jobTitle = "";
+		this.tedTask = "";
+		this.dirtyTask = "";
+		this.celeb = "";
+		this.uselessSkill = "";
+		this.obCeleb = "";
+		this.hugeNum = "";
+		this.adj = "";
+		this.isFormValid = true;
+		this.errorClass = ""
+		form.$setPristine();
+	}
+
+	this.interacted = function(form, field){
+		if(!form.$valid && form.$submitted){
+			this.errorClass = 'isNotValid';
+
+		}else if(form.$valid){
+			this.errorClass = '';
+		}
+		return form.$submitted && !field.$valid;
+	}
+
+	this.submit = function(form){
+		this.isFormValid = !form.$valid;
+	}
+
+	this.setMinNum = function(){
+		if(this.hugeNum < 0 || !this.hugeNum){
+			this.hugeNum = 10;
+		}
 	}
 
 });
